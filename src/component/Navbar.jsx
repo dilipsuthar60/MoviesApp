bfa13e4e633dfc5b253317920ce1f17eb63510f9
dilipsuthar60 <dilipsuthar212000@gamil.com>
2023-02-01 23:24:22 +0530

    improve navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div className='flex border items-center py-4 pl-5 space-x-10'>
         <img src={image} alt=""/>
         <div className='font-bold text-2xl text-blue-500 pointer'>
-        Movies
+        Movie
           </div>
         <div className='font-bold text-2xl text-blue-500 pointer'>Favourites</div>
     </div>
